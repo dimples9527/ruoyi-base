c0f25cc763425033c419601e72ad89459d295e83
dimples <1126834403@qq.com>
2021-03-04 10:48:45 +0800

    重新加入ruoyi的mapper包扫描

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/ApplicationConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.ruoyi.framework.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import java.util.TimeZone;
 @Configuration
 // 表示通过aop框架暴露该代理对象,AopContext能够访问
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan(basePackages = {"com.ruoyi.**.mapper"})
 public class ApplicationConfig {
     /**
      * 时区配置
